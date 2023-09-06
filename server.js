64227b48df82e0f55f94eb030da874361898b1ac
Bianca Chery <127991428+BiancaChery@users.noreply.github.com>
2023-09-06 01:38:07 +0000

    Added models

--- server.js
+++ server.js
@@ -11,6 +11,30 @@ const PORT = process.env.PORT;
 
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
 
+const signUp = require("./models/signUp");
+
+app.post("/signUp", async (req, res) => {
+  const { userName, firstName, lastName, password } = req.body;
+  const newSignUp = await signUp.create({ userName, firstName, lastName, password });
+  res.send(newSignUp);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 app.listen(PORT, () => {
      console.log(`${PORT}`);
    });
