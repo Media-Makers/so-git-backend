c9d9ce1474e67299e3065b4877cc667fd9631045
Bianca Chery <127991428+BiancaChery@users.noreply.github.com>
2023-09-06 01:12:02 +0000

    Added mongoose

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const cors = require("cors");
 const axios = require("axios");
+const mongoose = require("mongoose");
 
 const app = express();
 app.use(cors());
 
 require("dotenv").config();
 const PORT = process.env.PORT;
+
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
 
 app.listen(PORT, () => {
      console.log(`${PORT}`);
